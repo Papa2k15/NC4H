00eb2cc895a680d7c817b876bd5dd454ead9eb05
Gregory Daniels <danielslorenzoz2011@gmail.com>
2015-11-23 15:48:02 -0500

    Major 2.0 Update in progress

--- PortalProject/src/nc4h.py
+++ PortalProject/src/nc4h.py
@@ -7,12 +7,17 @@ import sqlite3 as lite
 import HTMLParser
 
 DATABASE = 'nc4h.db'
+nc4hers = ["Sarah Kotzian","Amy Chilcote","David Herpy Larry Hancock","Michael Yoder","Salim Oden","Shannon McCollum","Mitzi Downing"]
 
 nc4h = Flask(__name__)
 nc4h.secret_key = 'nc4h'
 
 @nc4h.route('/')
 def home():
+    return render_template('home.html')
+
+@nc4h.route('/add')
+def add_submission():
     return render_template('index.html')
 
 @nc4h.route('/getname/',methods=["GET"])
@@ -58,7 +63,7 @@ def sendtogreg():
             otherlinks.append(request.form[str(x)])
     print otherlinks
     store_info(wtd,nc4her,textsource,notes,medialinks,otherlinks,pagename)
-    return render_template('confirm.html',message="Successfully sent to Greg")
+    return render_template('confirm.html',message="Thats all you have to do, your information was successfully sent to Greg")
 
 @nc4h.route('/gregview/<pageid>', methods=["GET"])
 def myview(pageid=None):
@@ -130,10 +135,13 @@ def store_info(wtd,nc4her,textsource,notes,medialinks,otherlinks,pagename):
 def searchnc4hers():
     return render_template('search.html')
     
+@nc4h.route('/my_submissions')
+def mysubmissions():
+    return render_template("mysubmissions.html");
+    
 @nc4h.route('/finddata', methods=["GET"])
 def finddataforuser():
     nc4her = request.args.get('nc4her').replace("%20"," ")
-    print nc4her
     returnhtml = ""
     con = None
     try:
@@ -154,6 +162,99 @@ def finddataforuser():
     finally:
         if con:
             con.close()
-    
+
+@nc4h.route('/finddatat', methods=["GET"])
+def findmydata():
+    nc4her = request.args.get('nc4her').replace("%20"," ")
+    returnhtml = ""
+    con = None
+    try:
+        con = lite.connect(DATABASE)
+        with con:
+            con.rollback()
+            cur = con.cursor()
+            cur.execute("SELECT * FROM page WHERE nc4her = ?", (nc4her,))
+            con.commit()
+            data = cur.fetchall()
+            for x in data:
+                returnhtml += """
+                    <li class="list-group-item"><span><a href='/theirview/"""+str(x[0])+"""'>"""+str(x[0])+ " " + str(x[3]) + """</a></span></li>
+                """
+            return returnhtml
+    except lite.Error, e:
+        return str(e)
+    finally:
+        if con:
+            con.close()
+
+@nc4h.route('/remove_all', methods=['GET'])
+def removeall():
+    nc4her = request.args.get('nc4her').replace("%20"," ")
+    con = None
+    try:
+        con = lite.connect(DATABASE)
+        with con:
+            con.rollback()
+            cur = con.cursor()
+            cur.execute("SELECT * FROM page WHERE nc4her = ?", (nc4her,))
+            con.commit();
+            pageids = []
+            for x in cur.fetchall():
+                pageids.append(x[0])
+            
+            cur.execute("DELETE FROM page WHERE nc4her = ?", (nc4her,))
+            for ids in pageids:
+                cur.execute("DELETE FROM medialinks WHERE pageid = ?", (ids,))
+                cur.execute("DELETE FROM otherlinks WHERE pageid = ?", (ids,))
+            con.commit()
+            return "Removed All"
+    except lite.Error, e:
+        return str(e)
+    finally:
+        if con:
+            con.close()
+
+
+@nc4h.route('/theirview/<pageid>', methods=["GET"])
+def theirview(pageid=None):
+    con = None
+    try:
+        con = lite.connect(DATABASE)
+        with con:
+            con.rollback()
+            cur = con.cursor()
+            cur.execute("SELECT * FROM page WHERE pageid = ?", (pageid,))
+            con.commit()
+            pageinfo = cur.fetchone()
+            con.rollback()
+            cur = con.cursor()
+            cur.execute("SELECT * FROM medialinks WHERE pageid = ?", (pageid,))
+            con.commit()
+            medial = cur.fetchall()
+            con.rollback()
+            cur = con.cursor()
+            cur.execute("SELECT * FROM otherlinks WHERE pageid = ?", (pageid,))
+            con.commit()
+            otherl = cur.fetchall()
+            wtd = pageinfo[3]
+            nc4her = pageinfo[1]
+            textsource = pageinfo[4]
+            notes = pageinfo[5]
+            pagename = pageinfo[2]
+            medialinks = []
+            for m in medial:
+                medialinks.append(m[1])
+            otherlinks = []
+            for o in otherl:
+                otherlinks.append(o[1])
+            return render_template('managesubmissions.html',wtd=wtd,nc4her=nc4her,textsource=textsource,
+                           notes=notes,medialinks=medialinks,otherlinks=otherlinks,
+                           pagename=pagename)
+    except lite.Error, e:
+        return str(e)
+    finally:
+        if con:
+            con.close()
+
 if __name__ == '__main__':
     nc4h.run(host='0.0.0.0',port=2000,debug=True)
